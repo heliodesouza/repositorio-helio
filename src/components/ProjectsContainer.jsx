b49fbd3673968af4f2d5073e18be567c85705d0c
heliodesouza <heliodesouza088@gmail.com>
2024-09-21 19:36:01 -0300

    Atualizando o site 210924

--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -1,12 +1,17 @@
+import '../styles/components/projectsContainer.scss'
+
+
 const ProjectsContainer = () => {
     return (
       <section className="projects-container">
         <h2>Projetos</h2>
         <p>
-       
+         Durante meus estudos tenho feito alguns projetos através de cursos online em plataformas como; Udemy, DIO Curso em Vídeo.
+         Alguns desses projetos são; formulários, aplicações básicas de back-end e front-end. Tenho um longo caminho a percorrer, e estou muito animado quanto a isso.
         </p>
-         
+          
         <a  target="blank" href="https://glaucialisopremium.vercel.app/" className="btn">
+        Site de cabelereiro
         </a>
       </section>
     );
